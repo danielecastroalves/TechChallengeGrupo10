fa004fd63b9e64cd78d2725dae1dca08d7d72754
Daniele Garcia de Castro Alves <danielecastroalves@gmail.com>
2024-01-15 20:55:09 -0300

    criação do CommonController

---
+++ src/FintechGrupo10.WebApi/Controllers/Comum/CommonController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FintechGrupo10.WebApi.Controllers.Comum
+{
+    public abstract class CommonController : ControllerBase
+    {
+        protected readonly IMediator _mediator;
+
+        public CommonController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
