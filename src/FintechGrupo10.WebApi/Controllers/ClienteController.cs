101806e6749d36fe0799f1c386bf82064de3f062
Daniele Garcia de Castro Alves <danielecastroalves@gmail.com>
2024-01-15 20:55:09 -0300

    criação do CommonController

---
+++ src/FintechGrupo10.WebApi/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using FintechGrupo10.WebApi.Controllers.Comum;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace FintechGrupo10.WebApi.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+    public sealed class ClienteController : CommonController
+    {
+        public ClienteController(IMediator mediator) : base(mediator) { }
+
+        //[HttpGet]
+        //[SwaggerOperation(OperationId = "GetCliente")]
+        //public async Task<IActionResult> GetCliente()
+        //{
+
+        //}
+
+    }
+}
