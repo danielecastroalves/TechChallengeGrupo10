d3450ad7af146bc46d72a080febd5ad697ea0576
Daniele Garcia de Castro Alves <danielecastroalves@gmail.com>
2024-01-25 08:34:42 -0300

    feat: refactoring to include summary on LoginController

--- src/FintechGrupo10.WebApi/Controllers/LoginController.cs
+++ src/FintechGrupo10.WebApi/Controllers/LoginController.cs
@@ -6,15 +6,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FintechGrupo10.WebApi.Controllers
 {
+    /// <summary>
+    /// Login Controller
+    /// </summary>
     [ApiController]
-    [Route("login")]
+    [Route("v1/login")]
     public sealed class LoginController : CommonController
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mediator">Mediator DI</param>
         public LoginController(IMediator mediator) : base(mediator) { }
 
+        /// <summary>
+        /// Authenticate the user
+        /// </summary>
+        /// <param name="request">Login Request</param>
+        /// <param name="cancellationToken">Cancellation Token</param>
+        /// <returns></returns>
         [HttpPost]
         [AllowAnonymous]
-        public async Task<ActionResult<dynamic>> Autenticar(
+        public async Task<ActionResult<dynamic>> Authenticate(
             [FromBody] LoginRequest request,
             CancellationToken cancellationToken)
         {
--- tests/FintechGrupo10.Tests/UnitTests/Application/Recursos/PerguntasInvestimento/DeletaPerguntaTeste.cs
+++ tests/FintechGrupo10.Tests/UnitTests/Application/Recursos/PerguntasInvestimento/DeletaPerguntaTeste.cs
@@ -11,24 +11,24 @@ namespace FintechGrupo10.Tests.UnitTests.Application.Recursos.PerguntasInvestime
     {
         private readonly DeletaPerguntaHandler _handler;
         private readonly Mock<IRepository<Pergunta>> _repositorio;
-        private readonly DeletaPerguntaRequest _request;
 
         public DeletaPerguntaTeste()
         {
             var autoMock = new AutoMocker();
             _handler = autoMock.CreateInstance<DeletaPerguntaHandler>();
             _repositorio = autoMock.GetMock<IRepository<Pergunta>>();
-            _request = new DeletaPerguntaRequest();
         }
 
         [Fact]
         public async Task DeletaPerguntasDeInvestimento()
         {
             // Arrange
+            var request = new DeletaPerguntaRequest(Guid.NewGuid());
+
             _repositorio.Setup(x => x.DeleteByIdAsync(It.IsAny<Guid>(), CancellationToken.None)).ReturnsAsync(true);
 
             // Act
-            var result = await _handler.Handle(_request, CancellationToken.None);
+            var result = await _handler.Handle(request, CancellationToken.None);
 
             // Assert
             Assert.True(result);
